c819f396e529036a459b321d521b79ceef26d284
nasshmd <50814683+nasshmd@users.noreply.github.com>
2020-05-22 20:21:12 +0200

    Add files via upload

---
+++ lane detection.py
@@ -0,0 +1,439 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de
+# Barcelona (UAB).
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+# Keyboard controlling for CARLA. Please refer to client_example.py for a simpler
+# and more documented example.
+
+"""
+Welcome to CARLA manual control.
+
+Use ARROWS or WASD keys for control.
+
+    W            : throttle
+    S            : brake
+    AD           : steer
+    Q            : toggle reverse
+    Space        : hand-brake
+    P            : toggle autopilot
+
+    R            : restart level
+
+STARTING in a moment...
+"""
+
+from __future__ import print_function
+
+import cv2
+import argparse
+import logging
+import random
+import time
+
+try:
+    import pygame
+    from pygame.locals import K_DOWN
+    from pygame.locals import K_LEFT
+    from pygame.locals import K_RIGHT
+    from pygame.locals import K_SPACE
+    from pygame.locals import K_UP
+    from pygame.locals import K_a
+    from pygame.locals import K_d
+    from pygame.locals import K_p
+    from pygame.locals import K_q
+    from pygame.locals import K_r
+    from pygame.locals import K_s
+    from pygame.locals import K_w
+except ImportError:
+    raise RuntimeError('cannot import pygame, make sure pygame package is installed')
+
+try:
+    import numpy as np
+except ImportError:
+    raise RuntimeError('cannot import numpy, make sure numpy package is installed')
+
+from carla import image_converter
+from carla import sensor
+from carla.client import make_carla_client, VehicleControl
+from carla.planner.map import CarlaMap
+from carla.settings import CarlaSettings
+from carla.tcp import TCPConnectionError
+from carla.util import print_over_same_line
+
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+MINI_WINDOW_WIDTH = 600
+MINI_WINDOW_HEIGHT = 360
+
+def extrapolate_lines(lines, y_min, y_max):
+    slopes, intercepts = get_slope_intecept(lines)
+
+    new_lines = []
+
+    for slope, intercept, in zip(slopes, intercepts):
+        x1 = (y_min - intercept) / slope
+        x2 = (y_max - intercept) / slope
+        new_lines.append([x1, y_min, x2, y_max])
+
+    return np.array(new_lines)
+
+def get_slope_intecept(lines):
+    slopes = (lines[:, 3] - lines[:, 1]) / (lines[:, 2] - lines[:, 0] + 0.001)
+    intercepts = ((lines[:, 3] + lines[:, 1]) - slopes * (
+        lines[:, 2] + lines[:, 0])) / 2
+    return slopes, intercepts
+
+def vis_lanes(image_out, lane_lines):
+    for line in lane_lines:
+        x1, y1, x2, y2 = line.astype(int)
+
+        image_out = cv2.line(
+            image_out.astype(
+                np.uint8), (x1, y1), (x2, y2), (255, 0, 255), 2)
+    return image_out
+
+
+def merge_lane_lines(
+        lines):
+    """
+    Merges lane lines to output a single line per lane, using the slope and intercept as similarity measures.
+    Also, filters horizontal lane lines based on a minimum slope threshold.
+
+    Arguments:
+    lines -- tensor of dimension (N, 4) containing lines in the form of [x_1, y_1, x_2, y_2],
+    the coordinates of two points on the line.
+
+    Returns:
+    merged_lines -- tensor of dimension (N, 4) containing lines in the form of [x_1, y_1, x_2, y_2],
+    the coordinates of two points on the line.
+    """
+
+    ### START CODE HERE ### (≈ 25 lines in total)
+
+    # Step 0: Define thresholds
+    slope_similarity_threshold = 0.2
+    intercept_similarity_threshold = 20
+    min_slope_threshold = 0.3
+
+    # Step 1: Get slope and intercept of lines
+    slopes, intercepts = get_slope_intecept(lines)
+
+    # Step 2: Determine lines with slope less than horizontal slope threshold.
+    merged_lines = lines[abs(slopes) > min_slope_threshold]
+
+    # Step 3: Iterate over all remaining slopes and intercepts and cluster lines that are close to each other using a slope and intercept threshold.
+    for slope, intercept in zip(slopes, intercepts):
+        pass
+    # Step 4: Merge all lines in clusters using mean averaging
+
+    # Note: Make sure dimensions of returned lines is (N x 4)
+    ### END CODE HERE ###
+    return merged_lines
+
+
+def to_bgra_array(image):
+    """Convert a CARLA raw image to a BGRA numpy array."""
+    if not isinstance(image, sensor.Image):
+        raise ValueError("Argument must be a carla.sensor.Image")
+    array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+    array = np.reshape(array, (image.height, image.width, 4))
+    return array
+
+def make_carla_settings(args):
+    """Make a CarlaSettings object with the settings we need."""
+    settings = CarlaSettings()
+    settings.set(
+        SynchronousMode=False,
+        SendNonPlayerAgentsInfo=True,
+        NumberOfVehicles=15,
+        NumberOfPedestrians=30,
+        WeatherId=random.choice([1, 3, 7, 8, 14]),
+        QualityLevel=args.quality_level)
+    settings.randomize_seeds()
+    camera0 = sensor.Camera('CameraSemSeg', PostProcessing='SemanticSegmentation')
+    camera0.set_image_size(WINDOW_WIDTH, WINDOW_HEIGHT)
+    camera0.set_position(2.0, 0.0, 1.4)
+    camera0.set_rotation(0.0, 0.0, 0.0)
+    settings.add_sensor(camera0)
+
+    #camera2 = sensor.Camera('CameraSemSeg', PostProcessing='SemanticSegmentation')
+    #camera2.set_image_size(MINI_WINDOW_WIDTH, MINI_WINDOW_HEIGHT)
+    #camera2.set_position(2.0, 0.0, 1.4)
+    #camera2.set_rotation(0.0, 0.0, 0.0)
+    #settings.add_sensor(camera2)
+    return settings
+
+
+
+class Timer(object):
+    def __init__(self):
+        self.step = 0
+        self._lap_step = 0
+        self._lap_time = time.time()
+
+    def tick(self):
+        self.step += 1
+
+    def lap(self):
+        self._lap_step = self.step
+        self._lap_time = time.time()
+
+    def ticks_per_second(self):
+        return float(self.step - self._lap_step) / self.elapsed_seconds_since_lap()
+
+    def elapsed_seconds_since_lap(self):
+        return time.time() - self._lap_time
+
+
+class CarlaGame(object):
+    def __init__(self, carla_client, args):
+        self.client = carla_client
+        self._carla_settings = make_carla_settings(args)
+        self._timer = None
+        self._display = None
+        self._main_image = None
+        self._mini_view_image1 = None
+        self._mini_view_image2 = None
+        self._enable_autopilot = args.autopilot
+        self._lidar_measurement = None
+        self._map_view = None
+        self._is_on_reverse = False
+        self._display_map = args.map
+        self._city_name = None
+        self._map = None
+        self._map_shape = None
+        self._map_view = None
+        self._position = None
+        self._agent_positions = None
+
+    def execute(self):
+        """Launch the PyGame."""
+        pygame.init()
+        self._initialize_game()
+        try:
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+                self._on_loop()
+                self._on_render()
+        finally:
+            pygame.quit()
+
+    def _initialize_game(self):
+        self._on_new_episode()
+
+        if self._city_name is not None:
+            self._map = CarlaMap(self._city_name, 0.1643, 50.0)
+            self._map_shape = self._map.map_image.shape
+            self._map_view = self._map.get_map(WINDOW_HEIGHT)
+
+            extra_width = int((WINDOW_HEIGHT/float(self._map_shape[0]))*self._map_shape[1])
+            self._display = pygame.display.set_mode(
+                (WINDOW_WIDTH + extra_width, WINDOW_HEIGHT),
+                pygame.HWSURFACE | pygame.DOUBLEBUF)
+        else:
+            self._display = pygame.display.set_mode(
+                (WINDOW_WIDTH, WINDOW_HEIGHT),
+                pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+        logging.debug('pygame started')
+
+    def _on_new_episode(self):
+        self._carla_settings.randomize_seeds()
+        self._carla_settings.randomize_weather()
+        scene = self.client.load_settings(self._carla_settings)
+        if self._display_map:
+            self._city_name = scene.map_name
+        number_of_player_starts = len(scene.player_start_spots)
+        player_start = np.random.randint(number_of_player_starts)
+        print('Starting new episode...')
+        self.client.start_episode(player_start)
+        self._timer = Timer()
+        self._is_on_reverse = False
+
+    def _on_loop(self):
+        self._timer.tick()
+
+        measurements, sensor_data = self.client.read_data()
+
+        self._main_image = sensor_data.get('CameraSemSeg', None)
+        #self._mini_view_image2 = sensor_data.get('CameraSemSeg', None)
+
+        # Print measurements every second.
+
+        control = self._get_keyboard_control(pygame.key.get_pressed())
+        # Set the player position
+
+        if control is None:
+            self._on_new_episode()
+        elif self._enable_autopilot:
+            self.client.send_control(measurements.player_measurements.autopilot_control)
+        else:
+            self.client.send_control(control)
+
+    def _get_keyboard_control(self, keys):
+        """
+        Return a VehicleControl message based on the pressed keys. Return None
+        if a new episode was requested.
+        """
+        if keys[K_r]:
+            return None
+        control = VehicleControl()
+        if keys[K_LEFT] or keys[K_a]:
+            control.steer = -1.0
+        if keys[K_RIGHT] or keys[K_d]:
+            control.steer = 1.0
+        if keys[K_UP] or keys[K_w]:
+            control.throttle = 1.0
+        if keys[K_DOWN] or keys[K_s]:
+            control.brake = 1.0
+        if keys[K_SPACE]:
+            control.hand_brake = True
+        if keys[K_q]:
+            self._is_on_reverse = not self._is_on_reverse
+        if keys[K_p]:
+            self._enable_autopilot = not self._enable_autopilot
+        control.reverse = self._is_on_reverse
+        return control
+
+
+
+    def _on_render(self):
+        gap_x = (WINDOW_WIDTH - 2 * MINI_WINDOW_WIDTH) / 3
+        mini_image_y = WINDOW_HEIGHT - MINI_WINDOW_HEIGHT - gap_x
+        if self._main_image is not None:
+            array = to_bgra_array(
+                self._main_image)[:, :, 2]
+
+            result = np.zeros((array.shape[0], array.shape[1], 3))
+            result[np.where(array == 6)] = [255, 16, 59]
+            result[np.where(array == 8)] = [255, 10, 56]
+
+            kernel = np.ones((5, 5), np.uint8)
+            erosion = cv2.dilate(np.uint8(result), kernel, iterations=1)
+            dilate = cv2.erode(erosion, kernel, iterations=1)
+            edges = cv2.Canny(dilate, 50, 140)
+
+            # Step 3: Perform Line estimation using cv2.HoughLinesP()
+            params = dict(rho=10, theta=np.pi / 180, threshold=100, minLineLength=200, maxLineGap=50)
+            lines = cv2.HoughLinesP(edges, **params)
+
+            # Note: Make sure dimensions of returned lines is (N x 4)
+            ### END CODE HERE ###
+            if lines is not None:
+                lines = lines.reshape((-1, 4))
+
+                max_y = edges.shape[0]/2
+                min_y = max_y/2
+
+                merged_lines = merge_lane_lines(lines)
+                #extrapolated_lanes = extrapolate_lines(merged_lines, max_y, min_y)
+                result = vis_lanes(result, merged_lines)
+            surface = pygame.surfarray.make_surface(result.swapaxes(0, 1))
+            self._display.blit(surface, (0, 0))
+
+
+
+
+
+        if self._map_view is not None:
+            array = self._map_view
+            array = array[:, :, :3]
+
+            new_window_width = \
+                (float(WINDOW_HEIGHT) / float(self._map_shape[0])) * \
+                float(self._map_shape[1])
+            surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+
+            w_pos = int(self._position[0]*(float(WINDOW_HEIGHT)/float(self._map_shape[0])))
+            h_pos = int(self._position[1] *(new_window_width/float(self._map_shape[1])))
+
+            pygame.draw.circle(surface, [255, 0, 0, 255], (w_pos, h_pos), 6, 0)
+            for agent in self._agent_positions:
+                if agent.HasField('vehicle'):
+                    agent_position = self._map.convert_to_pixel([
+                        agent.vehicle.transform.location.x,
+                        agent.vehicle.transform.location.y,
+                        agent.vehicle.transform.location.z])
+
+                    w_pos = int(agent_position[0]*(float(WINDOW_HEIGHT)/float(self._map_shape[0])))
+                    h_pos = int(agent_position[1] *(new_window_width/float(self._map_shape[1])))
+
+                    pygame.draw.circle(surface, [255, 0, 255, 255], (w_pos, h_pos), 4, 0)
+
+            self._display.blit(surface, (WINDOW_WIDTH, 0))
+
+        pygame.display.flip()
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='CARLA Manual Control Client')
+    argparser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        dest='debug',
+        help='print debug information')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='localhost',
+        help='IP of the host server (default: localhost)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '-a', '--autopilot',
+        action='store_true',
+        help='enable autopilot')
+    argparser.add_argument(
+        '-l', '--lidar',
+        action='store_true',
+        help='enable Lidar')
+    argparser.add_argument(
+        '-q', '--quality-level',
+        choices=['Low', 'Epic'],
+        type=lambda s: s.title(),
+        default='Epic',
+        help='graphics quality level, a lower level makes the simulation run considerably faster')
+    argparser.add_argument(
+        '-m', '--map',
+        action='store_true',
+        help='plot the map of the current city')
+    args = argparser.parse_args()
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
+
+    logging.info('listening to server %s:%s', args.host, args.port)
+
+    print(__doc__)
+
+    while True:
+        try:
+
+            with make_carla_client(args.host, args.port) as client:
+                game = CarlaGame(client, args)
+                game.execute()
+                break
+
+        except TCPConnectionError as error:
+            logging.error(error)
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
